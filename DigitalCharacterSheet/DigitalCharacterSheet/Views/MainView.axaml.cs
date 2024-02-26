85abb53f069a8ec5e4d3103733b57c2de8007fff
Alex-Mayo <alexander.mayo@mymail.champlain.edu>
2024-02-26 13:32:12 -0500

    Create Empty Avalonia Project

---
+++ DigitalCharacterSheet/DigitalCharacterSheet/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace DigitalCharacterSheet.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
